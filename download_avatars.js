dba8ad6d9815123b56c596a50924da5fb087776e
gitmihalis <mihalisfthenos@icloud.com>
2017-07-03 22:52:59 +0000

    create the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const GITHUB_USER = "mihalis";
 const GITHUB_TOKEN = "2d82ba5a7ed26df0772a155798494a3d352561b0";
+
 console.log(`
 ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)
 Welcome to the GitHub Avatar Downloader!
@@ -17,8 +18,10 @@ Welcome to the GitHub Avatar Downloader!
 ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)
 `);
 
-function getRepoContributors(owner, name, cb) {
-
+function getRepoContributors(repoOwner, repoName, cb) {
+	const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + 
+		repoOwner + '/' + repoName + '/contributors';
+	console.log(requestURL);
 }
 getRepoContributors('infernojs', 'inferno', function(error, result) {
 	console.log('Errors: ' + error);
