c27d4049d14dc333e914831bc25a2b887371684e
gitmihalis <mihalisfthenos@icloud.com>
2017-07-03 23:34:48 +0000

    make the request to API

--- download_avatars.js
+++ download_avatars.js
@@ -1,29 +1,27 @@
 const request = require('request');
-const GITHUB_USER = "mihalis";
-const GITHUB_TOKEN = "2d82ba5a7ed26df0772a155798494a3d352561b0";
+const GITHUB_USER = "gitmihalis";
+const GITHUB_TOKEN = "0762d876dbbdb59e5cbebea36c05bdc6191cc27a";
 
 console.log(`
 ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)
-Welcome to the GitHub Avatar Downloader!
-───█───▄▀█▀▀█▀▄▄───▐█──────▄▀█▀▀█▀▄▄
-──█───▀─▐▌──▐▌─▀▀──▐█─────▀─▐▌──▐▌─█▀
-─▐▌──────▀▄▄▀──────▐█▄▄──────▀▄▄▀──▐▌
-─█────────────────────▀█────────────█
-▐█─────────────────────█▌───────────█
-▐█─────────────────────█▌───────────█
-─█───────────────█▄───▄█────────────█
-─▐▌───────────────▀███▀────────────▐▌
-──█──────────▀▄───────────▄▀───────█
-───█───────────▀▄▄▄▄▄▄▄▄▄▀────────█
+					Welcome to the GitHub Avatar Downloader!
 ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)
 `);
 
 function getRepoContributors(repoOwner, repoName, cb) {
-	const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + 
-		repoOwner + '/' + repoName + '/contributors';
-	console.log(requestURL);
+	const options = {
+		uri: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+		headers: { "User-Agent": "GitHub Avatar Downloader - Student Project" }
+	};
+	// request.get(options)
+	// 	.on('err', (err) => { throw err } )
+	// 	.on('response', (res) => { console.log(res.statusCode) });
+	request(options, function(err, res, body) {
+		cb(err, body)
+	})
+
 }
-getRepoContributors('infernojs', 'inferno', function(error, result) {
+getRepoContributors('jquery', 'jquery', function(error, result) {
 	console.log('Errors: ' + error);
 	console.log('Result: ' + result);
 });
